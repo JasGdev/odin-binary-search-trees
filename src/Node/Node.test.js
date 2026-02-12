b984d8d45fb6c4ea996c59d7a5bd500dec48377a
Jason <jason13.deng@gmail.com>
2026-02-12 10:07:39 +0900

    Setup test structure for Tree and Node

---
+++ src/Node/Node.test.js
@@ -0,0 +1,24 @@
+import { Node } from "./Node.js";
+
+let node
+
+beforeEach(() => {
+	node = new Node()
+});
+
+test("creating Linked list", () => {
+	// expect(list.headNode).toEqual(null);
+});
+
+describe("append() implementation", () => {
+	test("adding to Linked List x2", () => {
+		// list.append("key1", 5);
+		// list.append("key2", 7);
+
+		// const testNode2 = new Node("key2", 7);
+		// const testNode1 = new Node("key1", 5, testNode2);
+		// expect(list.headNode).toEqual(testNode1);
+		// expect(list.headNode.nextNode).toEqual(testNode2);
+	});
+});
+
