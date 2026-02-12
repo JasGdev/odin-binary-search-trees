fdce80b54501d039a68cb14077e3827922f53122
Jason <jason13.deng@gmail.com>
2026-02-12 11:34:25 +0900

    Implement buildTree() in Tree

--- src/Tree/Tree.js
+++ src/Tree/Tree.js
@@ -9,11 +9,15 @@ export class Tree {
 
 	static #buildTree(array) {
 		// returns the root node
-		let arr = [...new Set(array)].sort();
+		let arr = [...new Set(array)].sort((a,b) => a-b);
 
 		// use recursion to build tree from sorted array
 		// find middle element and make it root
 		// recursively repeat for left subarray and then right subarray
+
+        if (array.length == 0){
+            return null
+        }
 
 		let rootIndex = Math.floor(arr.length / 2);
 		let root = new Node(arr[rootIndex]);
--- src/Tree/Tree.test.js
+++ src/Tree/Tree.test.js
@@ -2,19 +2,43 @@ import { Tree } from "./Tree.js";
 
 let tree;
 
-beforeEach(() => {
-	// tree = new Tree()
+beforeEach(() => {});
+
+describe("logic implementation", () => {
+	test("Splitting left and right subtree", () => {
+		let arr = [0, 1, 2, 3, 4];
+		const rootIndex = 2;
+		let left = arr.slice(0, rootIndex);
+		let right = arr.slice(rootIndex + 1, arr.length);
+		expect(left).toEqual([0, 1]);
+		expect(right).toEqual([3, 4]);
+	});
+
+	test("sorting and removing duplicate from array", () => {
+		let array = [5, 1, 1, 0];
+		let arr = [...new Set(array)].sort((a,b) => a-b);
+		expect(arr).toEqual([0, 1, 5]);
+	});
 });
 
-test("Splitting left and right subtree", () => {
-	let arr = [0, 1, 2, 3, 4];
-	const rootIndex = 2;
-	let left = arr.slice(0, rootIndex);
-	let right = arr.slice(rootIndex + 1, arr.length);
-	expect(left).toEqual([0, 1]);
-	expect(right).toEqual([3, 4]);
-});
+describe("buildTree() implementation", () => {
+	test("buildTree() implementation for a 3 size tree with array length 4 ", () => {
+		let array = [5, 4, 4, 1];
+		let expectedListView = [4, [1, null, null], [5, null, null]];
 
-describe("append() implementation", () => {
-	test.skip("adding to Linked List x2", () => {});
+		tree = new Tree(array);
+		expect(tree.root.listView()).toEqual(expectedListView);
+	});
+
+	test("buildTree() implementation for a 5 size tree with array length 4 ", () => {
+		let array = [10, 5, 15, 3, 7, 12, 18, 1, 4, 6, 8, 11, 13, 17, 20, 7, 10, 12, 3];
+		let expectedListView = [
+			10,
+			[5, [3, [1, null, null], [4, null, null]], [7, [6, null, null], [8, null, null]]],
+			[15, [12, [11, null, null], [13, null, null]], [18, [17, null, null], [20, null, null]]],
+		];
+
+		tree = new Tree(array);
+		expect(tree.root.listView()).toEqual(expectedListView);
+	});
 });
